c78fe87d04f7d6c5025f262a4d90d647810f99ea
kevin-tian <kevin.tian@ibm.com>
2023-01-23 23:48:13 -0500

    add dropdown skeleton

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,6 +15,135 @@
       <img class="max-w-lg" src="../assets/main_page_holder.svg" />
     </div>
   </div>
+  <div class="mx-24 mt-20">
+    <p class="text-3xl my-8">Generate a codebase:</p>
+    <div class="flex mb-4">
+      <p class="text-2xl m-2">Website Type:</p>
+      <div class="dropdown">
+        <label tabindex="0" class="btn m-1"
+          >Landing Page
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 ml-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </label>
+
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>Landing Page</a></li>
+          <li><a>Portfolio</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <p class="text-2xl m-2">Framework:</p>
+      <div class="dropdown">
+        <label tabindex="0" class="btn m-1"
+          >Vue
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 ml-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </label>
+
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>Vue</a></li>
+          <li><a>Nuxt</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <p class="text-2xl m-2">UI Library:</p>
+      <div class="dropdown">
+        <label tabindex="0" class="btn m-1"
+          >Material UI
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 ml-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </label>
+
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>Material UI</a></li>
+          <li><a>Quasar</a></li>
+          <li><a>Tailwind</a></li>
+          <li><a>Bootstrap</a></li>
+          <li><a>None</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <p class="text-2xl m-2">Database/CMS:</p>
+      <div class="dropdown">
+        <label tabindex="0" class="btn m-1"
+          >None
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6 ml-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </label>
+
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li><a>None</a></li>
+          <li><a>Firestore</a></li>
+          <li><a>Supabase</a></li>
+          <li><a>Strapi</a></li>
+          <li><a>Contentful</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
