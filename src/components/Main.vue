fa7c8961d070b6267ca1a039c1803b91c404a095
kevin-tian <kevin.tian@ibm.com>
2023-01-24 21:30:14 -0500

    add refs to dropdowns

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,4 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const websiteType = ref("Landing Page");
+const framework = ref("Vue");
+const uiLibrary = ref("None");
+const backend = ref("None");
+const modal = ref(false);
+
+const chooseFramework = (value: string) => {
+  framework.value = value;
+  modal.value = true;
+  setTimeout(() => (modal.value = false), 500);
+};
+
+const chooseWebsiteType = (value: string) => {
+  websiteType.value = value;
+  modal.value = true;
+  setTimeout(() => (modal.value = false), 500);
+};
+
+const chooseUI = (value: string) => {
+  uiLibrary.value = value;
+  modal.value = true;
+  setTimeout(() => (modal.value = false), 500);
+};
+
+const chooseBackend = (value: string) => {
+  backend.value = value;
+  modal.value = true;
+  setTimeout(() => (modal.value = false), 500);
+};
+</script>
 
 <template>
   <div class="mt-32 mx-12 flex justify-around">
@@ -22,7 +53,7 @@
         <p class="text-xl m-1">Website Type:</p>
         <div class="dropdown">
           <label tabindex="0" class="btn btn-sm btn-primary mt-1"
-            >Landing Page
+            >{{ websiteType }}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -41,10 +72,13 @@
 
           <ul
             tabindex="0"
-            class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+            class="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52"
+            :class="{ hidden: modal }"
           >
-            <li><a>Landing Page</a></li>
-            <li><a>Portfolio</a></li>
+            <li>
+              <a @click="chooseWebsiteType('Landing Page')">Landing Page</a>
+            </li>
+            <li><a @click="chooseWebsiteType('Portfolio')">Portfolio</a></li>
           </ul>
         </div>
       </div>
@@ -52,7 +86,7 @@
         <p class="text-xl m-1">Framework:</p>
         <div class="dropdown">
           <label tabindex="0" class="btn btn-sm btn-primary mt-1"
-            >Vue
+            >{{ framework }}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -72,9 +106,10 @@
           <ul
             tabindex="0"
             class="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52"
+            :class="{ hidden: modal }"
           >
-            <li><a>Vue</a></li>
-            <li><a>Nuxt</a></li>
+            <li><a @click="chooseFramework('Vue')">Vue</a></li>
+            <li><a @click="chooseFramework('Nuxt')">Nuxt</a></li>
           </ul>
         </div>
       </div>
@@ -82,7 +117,7 @@
         <p class="text-xl m-1">UI Library:</p>
         <div class="dropdown">
           <label tabindex="0" class="btn btn-sm btn-primary mt-1"
-            >Material UI
+            >{{ uiLibrary }}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -102,12 +137,13 @@
           <ul
             tabindex="0"
             class="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52"
+            :class="{ hidden: modal }"
           >
-            <li><a>Material UI</a></li>
-            <li><a>Quasar</a></li>
-            <li><a>Tailwind</a></li>
-            <li><a>Bootstrap</a></li>
-            <li><a>None</a></li>
+            <li><a @click="chooseUI('None')">None</a></li>
+            <li><a @click="chooseUI('Vuetify')">Vuetify</a></li>
+            <li><a @click="chooseUI('Quasar')">Quasar</a></li>
+            <li><a @click="chooseUI('Tailwind')">Tailwind</a></li>
+            <li><a @click="chooseUI('Bootstrap')">Bootstrap</a></li>
           </ul>
         </div>
       </div>
@@ -115,7 +151,7 @@
         <p class="text-xl m-1">Database/CMS:</p>
         <div class="dropdown">
           <label tabindex="0" class="btn btn-sm btn-primary mt-1"
-            >None
+            >{{ backend }}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -135,17 +171,18 @@
           <ul
             tabindex="0"
             class="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52"
+            :class="{ hidden: modal }"
           >
-            <li><a>None</a></li>
-            <li><a>Firestore</a></li>
-            <li><a>Supabase</a></li>
-            <li><a>Strapi</a></li>
-            <li><a>Contentful</a></li>
+            <li><a @click="chooseBackend('None')">None</a></li>
+            <li><a @click="chooseBackend('Firestore')">Firestore</a></li>
+            <li><a @click="chooseBackend('Supabase')">Supabase</a></li>
+            <li><a @click="chooseBackend('Strapi')">Strapi</a></li>
+            <li><a @click="chooseBackend('Contentful')">Contentful</a></li>
           </ul>
         </div>
       </div>
     </div>
-    <div class="mockup-window border border-base-300 mt-12">
+    <div class="mockup-window border border-base-300 mt-8">
       <div class="flex justify-center px-4 py-16 border-t border-base-300">
         Hello!
       </div>
